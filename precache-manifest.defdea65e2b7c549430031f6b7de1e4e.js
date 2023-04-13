6f37a68f8cfa0802ca6c5c16a6367f80c29d03ea
Ishhit <satybaldiev.ishhit@gmail.com>
2023-04-13 16:48:15 +0500

    Updates

---
+++ precache-manifest.defdea65e2b7c549430031f6b7de1e4e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1c543f4c1890c3ee0e1",
+    "url": "/react-aktobe-gid/static/css/main.8ea5aa1d.chunk.css"
+  },
+  {
+    "revision": "f1c543f4c1890c3ee0e1",
+    "url": "/react-aktobe-gid/static/js/main.f1c543f4.chunk.js"
+  },
+  {
+    "revision": "aed29280cd3d845b9082",
+    "url": "/react-aktobe-gid/static/css/1.c73a64b5.chunk.css"
+  },
+  {
+    "revision": "aed29280cd3d845b9082",
+    "url": "/react-aktobe-gid/static/js/1.aed29280.chunk.js"
+  },
+  {
+    "revision": "bfb0a4a901225b2f5888",
+    "url": "/react-aktobe-gid/static/js/runtime~main.bfb0a4a9.js"
+  },
+  {
+    "revision": "776168780fe1c7f3a7c779141abcde59",
+    "url": "/react-aktobe-gid/static/media/nightlife.77616878.jpeg"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/react-aktobe-gid/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "0cd66fd58d3f28e51eb56ff3741c575e",
+    "url": "/react-aktobe-gid/static/media/detailsFood.0cd66fd5.jpeg"
+  },
+  {
+    "revision": "14c1ecb984e542580c4c5f1a4c42d7fe",
+    "url": "/react-aktobe-gid/static/media/food.14c1ecb9.jpg"
+  },
+  {
+    "revision": "02b7b30a5d3ebca414f4a583fe399a99",
+    "url": "/react-aktobe-gid/static/media/hotel1.02b7b30a.jpg"
+  },
+  {
+    "revision": "573a2b007fab33c348013839e430d48a",
+    "url": "/react-aktobe-gid/static/media/logos.573a2b00.svg"
+  },
+  {
+    "revision": "c4c1c5c962839661028a67435a863376",
+    "url": "/react-aktobe-gid/static/media/jeep.c4c1c5c9.mp4"
+  },
+  {
+    "revision": "e9b6a6ca5ef7c83762a6a782b360aec8",
+    "url": "/react-aktobe-gid/static/media/sokralnie_mesta.e9b6a6ca.jpg"
+  },
+  {
+    "revision": "38c5c87fd3b3b89ad1255fbb6c43d46b",
+    "url": "/react-aktobe-gid/static/media/dostoprimechatelnosti.38c5c87f.jpg"
+  },
+  {
+    "revision": "68487ff9a12b49901ed4d48ceaa6b86c",
+    "url": "/react-aktobe-gid/static/media/priroda.68487ff9.jpg"
+  },
+  {
+    "revision": "a2c5c24d4b8970a720cf0f7076939ac9",
+    "url": "/react-aktobe-gid/static/media/paralax.a2c5c24d.jpg"
+  },
+  {
+    "revision": "84a2b2e9fc69b8611f1b2a3fa50a5c01",
+    "url": "/react-aktobe-gid/index.html"
+  }
+];
